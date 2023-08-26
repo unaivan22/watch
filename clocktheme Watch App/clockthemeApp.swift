58a7db52a80113f8bf9b3ec88e6359f0aa929814
unaivan22 <unaivan22@gmail.com>
2023-08-26 20:22:31 +0700

    Initial Commit

---
+++ clocktheme Watch App/clockthemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  clockthemeApp.swift
+//  clocktheme Watch App
+//
+//  Created by unaivan on 26/08/23.
+//
+
+import SwiftUI
+
+@main
+struct clocktheme_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
